3c4f27f37ad01e86d9e5ada8b15b07e7a238c762
s.w.li <fouqau4@gmail.com>
2016-07-03 00:37:28 +0800

    update

---
+++ register.php
@@ -0,0 +1,40 @@
+<?php
+$con=mysqli_connect('localhost','root','nsysucse','login_file',8080);
+if ( mysqli_connect_error($con))
+{
+    echo "Failed to connect to MySQL : " . mysqli_connect_error() ;
+}
+else
+{
+    $noDup = TRUE;
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+  
+    $sql = 'select * from accountAndPassword';
+    $result = $con->query($sql);
+ 
+    while($row = mysqli_fetch_assoc($result) )
+    {
+        if( $row['account'] == $username )
+        {
+            $noDup = FALSE;
+            break;
+	}
+    }
+    if( $noDup )
+    {
+        $sql = "INSERT INTO accountAndPassword (account, password) values('{$username}','{$password}')";
+        if ($con->query($sql) === TRUE )
+        {
+            echo "yes";
+        }
+        else
+        {
+            echo "Error : " . $sql . "<br>" . $con->error;
+        }
+    }
+    else
+	echo "duplicate";
+}
+
+?>
