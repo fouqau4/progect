271f4abbcbdcf6e55bf5b642f2ae2f789c85b8de
fouqau4 <fouqau4@gmail.com>
2016-08-22 18:58:14 +0800

    update

--- recommand/recommand.m
+++ recommand/recommand.m
@@ -1,5 +1,4 @@
 load('parameter/best');
 data = load('data/mysuits_data');
-size(data)
-size(Theta1')
-sigmoid( [ ones( size( data, 1 ), 1 ) sigmoid( [ ones( size( data, 1 ), 1 ) data ] * Theta1' ) ] * Theta2' )
+result = sigmoid( [ ones( size( data, 1 ), 1 ) sigmoid( [ ones( size( data, 1 ), 1 ) data ] * Theta1' ) ] * Theta2' );
+save data/myscore result
